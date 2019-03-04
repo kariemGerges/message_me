ec7ea5738f351fb4a5616a7b9707b3299b5b8c60
kariem Gerges <kiko1086@gmail.com>
2019-03-04 10:00:55 -0500

    add flash msgs and login and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
 
+  def new
+    #code
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfuly logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something wrog with your cerditionals"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfuly logged out"
+    redirect_to login_path
+  end
+
 end
